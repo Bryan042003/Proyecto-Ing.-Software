4c0e50b7678d97447d3b93f2aefa2cb452fcc022
RichLeonC <r29leonc@gmail.com>
2024-03-12 21:53:40 -0600

    Inicializacion backend

---
+++ Backend/config/database.php
@@ -0,0 +1,50 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+class Database{
+
+    private static $instance = null;
+    private $conn;
+    private $host;
+    private $nameDB;
+    private $username;
+    private $password;
+    
+    private function __construct(){
+        $this->host = $_ENV['DB_HOST'];
+        $this->nameDB = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USERNAME'];
+        $this->password = $_ENV['DB_PASSWORD'];
+        
+        $this->conn = new PDO("mysql:host={$this->host};
+        dbname={$this->nameDB}", 
+        $this->username, $this->password);
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+
+    public function testConnection() {
+        try {
+            $this->conn->query("SELECT 1");
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
+
+?>
---
+++ Backend/modelos/Audio.php
@@ -0,0 +1,30 @@
+<?php
+
+class Audio{
+    private $conn;
+    private $vista = "audio";
+
+    public $titulo;
+    public $autor;
+    public $comentarios;
+    public $ruta_audio;
+    public $ruta_imagen;
+    public $latitud;
+    public $longitud;
+    public $distrito;
+    public $canton;
+    public $provincia;
+
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function obtenerAudios(){
+        $query = "SELECT * FROM " . $this->vista;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+}
+?>
