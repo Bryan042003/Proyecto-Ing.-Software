eb4155feca43d27d6d0d664cd6a77e1d3e81b009
Bryan042003 <80810476+Bryan042003@users.noreply.github.com>
2024-03-12 18:01:47 -0600

    Add new routes to app-routing module

--- Frontend/sonidos_del_pura_vida/src/app/app-routing.module.ts
+++ Frontend/sonidos_del_pura_vida/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { LoginComponent } from './login/login.component';
+import { VistaAdministradorComponent } from './vista-administrador/vista-administrador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: "pagina-principal", pathMatch: "full"},
+  {path: 'pagina-principal',component: PaginaPrincipalComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'vista-administrador',component: VistaAdministradorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
