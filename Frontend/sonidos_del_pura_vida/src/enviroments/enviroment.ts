192e51aebd366503692eb6282d4ffc200b7fa670
RichLeonC <r29leonc@gmail.com>
2024-04-08 12:18:56 -0600

    Update enviroment.ts

--- Frontend/sonidos_del_pura_vida/src/enviroments/enviroment.ts
+++ Frontend/sonidos_del_pura_vida/src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    //baseUrl: 'http://localhost/sonidosPV/'
-    baseUrl: 'http://129.213.52.156/sonidosPV/Backend/'
+    baseUrl: 'http://localhost/sonidosPV/'
+    // baseUrl: 'http://129.213.52.156/sonidosPV/Backend/'
   };
   
