ef6ca5df238f784e0ff976f5007c9fde431f38a7
Bryan042003 <80810476+Bryan042003@users.noreply.github.com>
2024-04-10 20:32:19 -0600

    Terminado seccioón de ayuda y mostrar cards de audios  corregidos

--- Frontend/sonidos_del_pura_vida/src/app/Componentes/card-audio/card-audio.component.ts
+++ Frontend/sonidos_del_pura_vida/src/app/Componentes/card-audio/card-audio.component.ts
@@ -1,38 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { PasarDatosService } from '../../services/pasar-datos.service';
+import { Component, Input } from '@angular/core';
 import { Audio } from '../../models/Audio.model';
-
 
 @Component({
   selector: 'app-card-audio',
   templateUrl: './card-audio.component.html',
-  styleUrl: './card-audio.component.css'
+  styleUrls: ['./card-audio.component.css']
 })
-export class CardAudioComponent implements OnInit{
-
-  audios:Audio[] = [];
-  constructor(private pasarDatosService: PasarDatosService) { }
-
-  ngOnInit(): void {
-    this.cargarAudios();
-  }
-
-  private cargarAudios(){
-    this.pasarDatosService.getAudios().subscribe(
-      (res:any) => {
-        console.log(res);
-        this.audios = res;
-      }
-    );
-  }
-
-/*
-  deleteAudio(id:number){
-    this.pasarDatosService.deleteAudio(id).subscribe();
-    this.audios = this.audios.filter(audio => audio.id !== id);
-  }
-
-
-*/
+export class CardAudioComponent {
+  @Input() audio!: Audio;
 
 }
--- Frontend/sonidos_del_pura_vida/src/app/pagina-principal/pagina-principal.component.ts
+++ Frontend/sonidos_del_pura_vida/src/app/pagina-principal/pagina-principal.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PasarDatosService } from '../services/pasar-datos.service';
+import { Audio } from '../models/Audio.model';
 
 @Component({
   selector: 'app-pagina-principal',
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
-export class PaginaPrincipalComponent {
+export class PaginaPrincipalComponent implements OnInit {
+  audios:Audio[] = [];
 
+  constructor(private pasarDatosService: PasarDatosService) { }
+
+  ngOnInit(): void {
+    this.cargarAudios();
+  }
+
+  private cargarAudios(){
+    this.pasarDatosService.getAudios().subscribe(
+      (res:any) => {
+        console.log(res);
+        this.audios = res;
+      }
+    );
+  }
+
+/*
+  deleteAudio(id:number){
+    this.pasarDatosService.deleteAudio(id).subscribe();
+    this.audios = this.audios.filter(audio => audio.id !== id);
+  }
+
+
+*/
 }
