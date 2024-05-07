1cc8f0f384ed4168c2535143d9a7019386edf4e4
RichLeonC <r29leonc@gmail.com>
2024-05-07 16:15:37 -0600

    Form group login

--- Frontend/sonidos_del_pura_vida/src/app/login/login.component.ts
+++ Frontend/sonidos_del_pura_vida/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { PasarDatosService } from '../services/pasar-datos.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private fb: FormBuilder,public pasarDatosService: PasarDatosService) { }
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    console.log('Formulario enviado');
+  }
 
 }
