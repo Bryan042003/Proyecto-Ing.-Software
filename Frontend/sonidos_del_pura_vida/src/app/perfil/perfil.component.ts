01979aa9fd449c4076da2bfc55324e4647098448
RichLeonC <r29leonc@gmail.com>
2024-05-26 20:55:56 -0600

    Avance perfil

--- Frontend/sonidos_del_pura_vida/src/app/perfil/perfil.component.ts
+++ Frontend/sonidos_del_pura_vida/src/app/perfil/perfil.component.ts
@@ -11,4 +11,16 @@ export class PerfilComponent {
   constructor(private pasarDatosService:PasarDatosService){}
 
   admin = this.pasarDatosService.getAdminFromToken();
+
+  esEditable = false;
+  nombre = this.admin.nombre;
+
+  guardarCambios(){
+    alert('Cambios guardados')
+    this.toggleEditable();
+  }
+
+  toggleEditable(){
+    this.esEditable = !this.esEditable;
+  }
 }
